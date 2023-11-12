db9bf4cf8946b85395271521b4ef3606ab8a9cdf
Ryan <ryan@ryansolomon.io>
2023-11-12 07:28:58 -0700

    update base url

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -11,7 +11,7 @@ export default function AuthForm(){
         view="magic_link"
         showLinks={false}
         providers={[]}
-        redirectTo='http://localhost:3000/auth/callback'
+        redirectTo='https://watch-list-plum.vercel.app/auth/callback'
         appearance={{
             theme: 'dark',
             button: {
